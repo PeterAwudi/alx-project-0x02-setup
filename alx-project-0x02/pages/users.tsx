808e76c92735060a0f82a83b094621b6e1bb92ed
PeterAwudi <awudipeter20@gmail.com>
2025-06-07 13:30:41 +0000

    Updated Header component

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,24 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import UserCard from '../components/common/UserCard';
 
-const Users = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setUsers(data));
-  }, []);
-
+const Users = ({ users }) => {
   return (
     <div className="max-w-3xl mx-auto p-4">
       {users.map(user => (
-        <UserCard key={user.id} id={user.id} name={user.name} email={user.email} address={user.address} />
+        <UserCard
+          key={user.id}
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          address={user.address}
+        />
       ))}
     </div>
   );
 };
 
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
+
 export default Users;
+
 
 
