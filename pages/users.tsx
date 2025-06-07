60fc3f9295fdf76849b22b529f4c10d64c940524
PeterAwudi <awudipeter20@gmail.com>
2025-06-07 14:23:56 +0000

    Updated Header component

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,5 +1,6 @@
 import UserCard from '../components/common/UserCard';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
 interface Address {
   street: string;
   suite: string;
@@ -14,15 +15,7 @@ interface User {
   address: Address;
 }
 
-const Users = () => {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(data => setUsers(data as User[]));
-  }, []);
-
+const Users = ({ users }: { users: User[] }) => {
   return (
     <div className="max-w-3xl mx-auto p-4">
       {users.map((user: User) => (
@@ -38,5 +31,17 @@ const Users = () => {
   );
 };
 
+export const getServerSideProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
+
 export default Users;
+
 
